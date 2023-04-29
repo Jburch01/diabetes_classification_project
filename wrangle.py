1fca133de34517cc19e68e3910915d611052ef24
Josh Burch <joshuaburch01@gmail.com>
2023-04-28 22:16:11 -0500

    updated function

--- wrangle.py
+++ wrangle.py
@@ -10,8 +10,19 @@ def get_data():
         df = pd.read_csv('diabetes_prediction_dataset.csv')
         df.drop_duplicates(ignore_index=True, inplace=True)
         df = df.drop(df[df.age < 1].index)
+        df.age = df.age.astype(int)
         df = df.rename(columns={'diabetes':'diabetic'})
+        mask = df.gender == 'Other'
+        df = df.drop(df[mask].index)
+        bins = [0, 18, 29, 39, 49, 59, 69, 80]
+        #labels = ['1-18', '19-29', '30-39', '40-49', '50-59', '60-69', "70+"]
+        labels = [1, 2, 3, 4, 5, 6, 7]
+        df['age_bin'] = pd.cut(df['age'], bins=bins, labels=labels)
+        bmi_labels = [1, 2, 3, 4, 5, 6]
+        bmi_bins= [0, 18.5, 25, 30, 35, 40, 100]
+        df['bmi_class'] = pd.cut(df.bmi, bins=bmi_bins, labels=bmi_labels)
         return df
+    
     
     
 def train_validate_test(df,target):
